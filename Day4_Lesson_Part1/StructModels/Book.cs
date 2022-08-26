c350491086055624e65821c81a0b32ca81fae59f
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-26 16:29:30 +0400

    tasks ready

---
+++ Day4_Lesson_Part1/StructModels/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_Lesson_Part1.StructModels
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string WriterName  { get; set; }
+        public string BookName { get; set; }
+        public int PageCount { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+    public  struct Kitab
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string WriterName { get; set; }
+        public string BookName { get; set; }
+        public int PageCount { get; set; }
+        public DateTime CreatedDate { get; set; }   
+    }
+}
---
+++ Day4_Lesson_Part2/Models/Hotel.cs
@@ -0,0 +1,44 @@
+﻿using Day4_Lesson_Part2.Models.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_Lesson_Part2.Models
+{
+    public class Hotel
+    {
+        public Hotel(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        private Room[] Rooms =new Room[0];
+        public void AddRoom(Room room) {
+            Array.Resize<Room>(ref Rooms, Rooms.Length + 1);
+            Rooms[Rooms.Length - 1] = room;
+        }
+        public bool MakeReservation(int roomId)
+        {
+            var result = Array.Find(Rooms, r => r.Id == roomId);
+            if ( result==null)
+            {
+                throw new NotAviableException("Bu otaq yoxdur");
+            }
+            else
+            {
+                if (!result.IsAviable)
+                {
+                    throw new Exception("Bu otaq bos deyil");
+                }
+                else
+                {
+                    Console.WriteLine(result.IsAviable);
+                    return result.IsAviable;
+                }
+               
+            }
+        }
+    }
+}
