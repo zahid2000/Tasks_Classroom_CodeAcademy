869ace577d3128e5b660185af1a97ed508b79325
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-29 16:14:08 +0400

    part 2 finish

---
+++ Day5_Lesson_Part2/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Day5_Lesson_Part2.Services;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+
+string[] Urls = { "https://www.codeproject.com/Questions/204778/Get-HTML-code-from-a-website-C", 
+    "https://classroom.google.com/u/0/c/NTI2NjYzMzkxMDI2", "https://en.wikipedia.org/wiki/URL" ,
+"https://docs.microsoft.com/tr-tr/dotnet/api/system.collections.generic.sorteddictionary-2?view=net-6.0",
+"https://docs.microsoft.com/tr-tr/dotnet/api/system.collections.generic.sorteddictionary-2.enumerator?view=net-6.0"
+};
+
+List<Task<string>> tasks = new List<Task<string>>();
+var finished=
+foreach (var item in tasks)
+{
+    
+}
+Stopwatch stopWatch=new Stopwatch();
+HttpClient client = new HttpClient();
+#region forma1
+Console.WriteLine("Http Web Request");
+foreach (var item in Urls)
+{
+    stopWatch.Restart();
+    var count = await HtmlService.CountAsync(item);
+    Console.WriteLine($"{count}--{stopWatch.ElapsedMilliseconds}--seconds");
+}
+stopWatch.Stop();
+
+Console.WriteLine(new string('_',50));
+#endregion
+
+#region forma2
+Console.WriteLine("Http Client");
+stopWatch.Start();
+foreach (var item in Urls)
+{
+    stopWatch.Restart();
+
+    string html = await client.GetStringAsync(item);
+    Console.WriteLine($"{html.Length}--{stopWatch.ElapsedMilliseconds}--seconds");
+
+}
+
+stopWatch.Stop();
+Console.ReadLine();
+#endregion
---
+++ Day5_Lesson_Part2/Services/HtmlService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Lesson_Part2.Services
+{
+    public class HtmlService
+    {
+        public static async Task<int> CountAsync(string Url)
+        {
+
+            HttpWebRequest myRequest = (HttpWebRequest)HttpWebRequest.Create(Url);
+            myRequest.Method = "GET";
+            WebResponse myResponse =await myRequest.GetResponseAsync();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+            return result.Count();
+        }
+        public static int Count(string Url)
+        {
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(Url);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string result = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+            return result.Count();
+        }
+        static int cc(string url){
+            HttpWebRequest request =(HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Get";
+            WebResponse response = request.GetResponse();
+            StreamReader sr=new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
+            string htmlresult=sr.ReadToEnd();
+            sr.Close();
+            response.Close();
+            return htmlresult.Count();
+        }
+
+
+
+}
+}
