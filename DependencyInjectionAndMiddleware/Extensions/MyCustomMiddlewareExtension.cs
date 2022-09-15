8239c9b0f6d0f4e33c34565e074ef97e692007a0
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-09-15 11:09:55 +0400

    day 20  part 1 end

---
+++ DependencyInjectionAndMiddleware/Extensions/MyCustomMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using DependencyInjectionAndMiddleware.Middlewares;
+
+namespace DependencyInjectionAndMiddleware.Extensions
+{
+    public static class MyCustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UserMyFirstMIddleware(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<FirstCustomMiddleware>();
+        }
+    }
+}
---
+++ DependencyInjectionAndMiddleware/Middlewares/FirstCustomMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace DependencyInjectionAndMiddleware.Middlewares
+{
+    public class FirstCustomMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        public FirstCustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            
+            Console.WriteLine("FirstCustomMiddleware -> Start (Hello World)");
+            await _next.Invoke(context);
+
+            Console.WriteLine("FirstCustomMiddleware -> End (Hello World)");
+          
+        }
+
+    }
+}
