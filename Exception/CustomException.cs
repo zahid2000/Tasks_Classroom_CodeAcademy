aecb49db24faa2740d36282b6b802c14f05b8d0f
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-09-19 08:20:56 +0400

    Last

---
+++ Exception/CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    public class CustomException:System.Exception
+    {
+        public CustomException():base()
+        {
+            
+        }
+        public CustomException(string message):base(message)
+        {
+            
+        }
+    }
+}
---
+++ Exception/Program.cs
@@ -0,0 +1,39 @@
+﻿using Exception;
+using Microsoft.CSharp.RuntimeBinder;
+
+dynamic a = "nhh";
+//Console.WriteLine(typeof(DivideByZeroException));
+try
+{
+    int num = a;
+    int num1 = 5;
+    int num2 = 0;
+    int num3 = num1 / num2;
+
+    int[] arr = new int[0];
+    arr[0] = 7;
+}
+catch (System.Exception e) when (e.GetType() != typeof(RuntimeBinderException) && e.GetType() != typeof(DivideByZeroException))
+{
+    Console.WriteLine("Error");
+}
+catch (DivideByZeroException e)
+{
+
+    Console.WriteLine("0 a bole bilmezsiz");
+
+
+}
+catch (RuntimeBinderException e)
+{
+    Console.WriteLine("Incorrect cast ");
+    throw new CustomException("Error");
+}
+finally
+{
+    Console.WriteLine("Finally block is read");
+}
+
+
+
+
