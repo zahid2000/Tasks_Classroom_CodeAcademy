2fdf474e5072760f8207d14b58699b1f38719741
zahid2000 <zahidmamedov07@gmail.com>
2022-10-11 11:10:18 +0400

    rabbit mq

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+const string queue_name = "code-queue";
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Uri=new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel=connection.CreateModel();
+channel.QueueDeclare(queue_name,durable:true,exclusive:false,autoDelete:false,arguments:null);
+
+
+var consumer=new EventingBasicConsumer(channel);
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var body=e.Body.ToArray();
+    var message=Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+    Thread.Sleep(1500);
+}
+
+channel.BasicConsume(queue_name, true, consumer);
+Console.WriteLine("Consume Started");
+
+Console.ReadKey();
---
+++ RabbitMQ/RabbitMQ.Preducer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+const string queue_name = "code-queue";
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue_name, durable: true, exclusive: false, autoDelete: false, arguments: null);
+int i = 0;
+while (true)
+{
+    var message = new
+    {
+        Name = "Producer",
+        Message = "Console Message -> Code Academy ",
+        MessageId = i++
+    };
+    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+    channel.BasicPublish("", queue_name, null, body);
+}
+
